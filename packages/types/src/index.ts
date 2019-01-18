6cbb50bf1fa9af2a3ce1840cf33518db407dfc14
Rick Heere <rickheere@gmail.com>
2019-01-18 12:25:49 +0100

    Add a package for the types

---
+++ packages/types/src/index.ts
@@ -0,0 +1,28 @@
+export interface Config {
+  baseUrl: string;
+}
+
+export interface Paginated {
+  offset: number;
+  limit: number;
+}
+
+export interface PaginatedResponse extends Paginated {
+  hasMore: number;
+}
+
+export interface Show {
+  id: number;
+  name: string;
+}
+
+export interface Person {
+  id: number;
+  name: string;
+  birthday: string;
+}
+
+export interface showAndCastResource {
+  getShows(page: number): Show[];
+  getCastByShowId(id: number): Person[];
+}
